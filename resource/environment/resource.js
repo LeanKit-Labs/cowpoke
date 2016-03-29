603ff93fa77b2c8bf1e30a46f86aba7c3b7aaeff
EvanSnapp <evan.snapp@leankit.com>
2016-03-29 15:29:44 -0500

    Fixed error with multiple copies of a service in different stacks, added optional authorization, and updated tests

--- resource/environment/resource.js
+++ resource/environment/resource.js
@@ -7,7 +7,7 @@ var format = require( "util" ).format;
 
 function checkAuth(envelope, next) {
     var userKey = envelope.headers.bearer;
-    if (userKey === process.env.COWPOKE_API_KEY){
+    if (!process.env.COWPOKE_API_KEY || userKey === process.env.COWPOKE_API_KEY){
         return next();
     } else {
         return { status: 402, data: { message: "Unauthorized" } };
