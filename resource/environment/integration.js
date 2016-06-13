75c26f3de7b2f9d896380790a144fa9c42ae8166
EvanSnapp <evan.snapp@leankit.com>
2016-06-13 12:36:34 -0500

    fixed status code

--- resource/environment/integration.js
+++ resource/environment/integration.js
@@ -212,7 +212,7 @@ function upgrade( slack, envelope ) {
 	var image = envelope.data.image;
 	if ( !util.checkInfo( util.getImageInfo( image ) ) ) { //check tag if tag is formated correctly
 		return {
-			status: 500,
+			status: 400,
 			data: {
 				message: "Invalid Image (" + image + "). Expected tag to be formatted by buildgoggles."
 			}
