1538baa7563c4eeaa93e3bd0de872f4f0c098a68
Shawn Hartsell <shawn.hartsell@leankit.com>
2016-06-28 11:44:38 -0500

    Revert "Feature poll docker"

--- src/index.js
+++ src/index.js
@@ -7,24 +7,16 @@ var config = require( "configya" )( {
 	file: "./config.json"
 } );
 var fount = require( "fount" );
-var _ = require( "lodash" );
 
-if ( !config.docker.user || !config.docker.pass ) {
+if ( !process.env.DOCKER_USER || !process.env.DOCKER_PASS ) {
 	throw new Error( "DOCKER_PASS or DOCKER_USER is not defined" );
-}
-
-if ( !_.isNumber( config.docker.poll.time ) || !_.isNumber( config.docker.poll.interval ) ) {
-	throw new Error( "ERROR: docker poll time and/or interval is not numeric" );
 }
 
 var environments = require( "./data/nedb/environment" );
 fount.register( "environment", environments );
 
 var slack = require( "./slack" )( config.slack.token, environments );
-var dockerhub = require( "./dockerhub" )( config.docker.user, config.docker.pass,
-	config.docker.poll.time, config.docker.poll.interval );
 fount.register( "slack", slack );
-fount.register( "dockerhub", dockerhub );
 
 var host = hyped.createHost( autohost, {
 	port: config.host.port,
